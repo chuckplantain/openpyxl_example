f3b13845b055afc5ae6b2c0632fdb0d4e08f2c76
Kyle Blaze Petan <kyleblazepetan@gmail.com>
2015-10-16 14:02:17 -0400

    not working, only adding one segment when should be > 1

--- mail_merge.py
+++ mail_merge.py
@@ -3,26 +3,40 @@ import re
 
 def process_merge(data, flightSegmentData):
     """
-
     :param data: a dict of values, keys being the <<Values>> in lowercase form
-    :param template_file: the file with the template (.xlsx)
-    :param new_file: A place to save the newFile... ends in .xlsx and destructively saves
+    :param flightSegmentData: array of dicts corresponding to flight legs
     :return: True if all went well
     """
     pattern = re.compile(r'(<<)(\w+)(>>)')
     wb = load_workbook('/Users/kyleblazepetan/Desktop/mailMergeTemplate.xlsx')
     ws = wb.active
 
-    def lookUpFunction(matchobj):
-        resp = data[str(matchobj.group(2)).lower()]
-        if resp:
-            return resp
-        return 'key not found'
+    def buildForm(segments):
+        pertinent_rows = ws['A32':'L54']
+        start = 56
+        for i in range(1, segments + 2):
+            for rowidx, rows in enumerate(pertinent_rows, start=start):
+                for colidx, template_cell in enumerate(rows, start=1):
+                    current_cell = ws.cell(row = rowidx, column = colidx)
+                    copied_style = template_cell.style
+                    current_cell.value = template_cell.value
+                    current_cell.style = copied_style
+                    start += 25
 
-    for row in ws.iter_rows():
-        for cell in row:
-            if cell.value is not None:
-                cell.value = re.sub(pattern, lookUpFunction, cell.value.lower())
+    flightSegments = len(flightSegmentData)
+    if flightSegments > 1:
+        buildForm(flightSegments)
 
-    wb.save('/Users/kyleblazepetan/Desktop/maergeTesting.xlsx')
+#    def lookUpFunction(matchobj):
+#        resp = data[str(matchobj.group(2)).lower()]
+#        if resp:
+#            return resp
+#        return 'key not found'
+#
+#    for row in ws.iter_rows():
+#        for cell in row:
+#            if cell.value is not None:
+#                cell.value = re.sub(pattern, lookUpFunction, cell.value.lower())
+#
+    wb.save('/Users/kyleblazepetan/Desktop/mergeTesting.xlsx')
     return True
