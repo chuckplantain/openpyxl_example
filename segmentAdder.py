43b123c418dc5f487d303415767e5a641d8e9e21
Kyle Blaze Petan <kyleblazepetan@gmail.com>
2015-10-19 15:50:12 -0400

    working flightSegment form builder

--- segmentAdder.py
+++ segmentAdder.py
@@ -2,24 +2,32 @@ from openpyxl import load_workbook
 from itertools import *
 import re
 
-pattern = re.compile(r'(Flight Segment )(\d*)')
-
-def segment_repl(matchobj):
-    base = str(matchobj.group(0))
-    digits = str(matchobj.group(1))
-    return base + ' ' + '34343'
-
 wb = load_workbook('/Users/kyleblazepetan/Desktop/mailMergeTemplate.xlsx')
 ws = wb.active
-segments = 55 + ( 23 * 3 )
-for index in xrange(55, segments, 23):
-    pertinent_rows = ws['A32':'L54']
-    blank_rows = ws.iter_rows(row_offset=index)
-    for row, bl_row in zip(pertinent_rows, blank_rows):
-        for cell, bl_cell in zip(row, bl_row):
-            desired_style = cell.style
-            if cell.value is not None:
-                bl_cell.value = re.sub(pattern, segment_repl, cell.value)
-                bl_cell.style = desired_style
+pattern = re.compile(r'FLIGHT SEGMENT ')
 
-wb.save('/Users/kyleblazepetan/Desktop/segements.xlsx')
+def segmentAdder(flightSegments):
+
+    passedin = len(flightSegments)
+    segments = 55 + ( 23 * passedin )
+    for index in xrange(55, segments, 23):
+        crazy_inner_count = (index - 55) / 23 + 2
+        pertinent_rows = ws['A32':'L54']
+        blank_rows = ws.iter_rows(row_offset=index)
+        for row, bl_row in zip(pertinent_rows, blank_rows):
+            for cell, bl_cell in zip(row, bl_row):
+                desired_style = cell.style
+                bl_cell.style = desired_style
+                if cell.value is not None:
+                    val = cell.value
+                    if pattern.search(val) is not None:
+                        print('real_good')
+                        bl_cell.value = "Flight Segment " +  str(crazy_inner_count)
+                        print crazy_inner_count
+                        print 'foo'
+                    else:
+                        bl_cell.value = cell.value
+                else:
+                    bl_cell.value = cell.value
+
+    wb.save('/Users/kyleblazepetan/Desktop/segements.xlsx')
