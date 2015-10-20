c7953b557dbccf2ec9e2e8b242691335a241c1a6
Kyle Blaze Petan <kyleblazepetan@gmail.com>
2015-10-20 09:21:06 -0400

    need to connect the two

--- mail_merge.py
+++ mail_merge.py
@@ -1,7 +1,8 @@
+from segmentAdder import segmentAdder
 from openpyxl import load_workbook
 import re
 
-def process_merge(data, flightSegmentData):
+def process_merge(data, flightSegments):
     """
     :param data: a dict of values, keys being the <<Values>> in lowercase form
     :param flightSegmentData: array of dicts corresponding to flight legs
@@ -10,45 +11,19 @@ def process_merge(data, flightSegmentData):
     pattern = re.compile(r'(<<)(\w+)(>>)')
     wb = load_workbook('/Users/kyleblazepetan/Desktop/mailMergeTemplate.xlsx')
     ws = wb.active
+    segments = len(flightSegments)
 
-    def zipForm(segments):
-        pertinent_rows = ws['A32':'L54']
-        for templateRow, blankRow in zip(pertinent_rows, ws.iter_rows(row_offset=start_row)):
-            for template_cell, current_cell in zip(templateRow, blankRow):
-                copied_style = template_cell.style
-                current_cell.value = template_cell.value
-                current_cell.style = copied_style
+    def lookUpFunction(matchobj):
+        resp = data[str(matchobj.group(2)).lower()]
+        if resp:
+            return resp
+        return 'key not found'
+    
+    segmentAdder(segments, ws)
 
+    for row in ws.iter_rows():
+        for cell in row:
+            if cell.value is not None:
+                cell.value = re.sub(pattern, lookUpFunction, cell.value.lower())
 
-    def buildForm(segments):
-        pertinent_rows = ws['A32':'L54']
-        start = 56
-        for i in range(1, segments + 2):
-            for rowidx, rows in enumerate(pertinent_rows, start=start):
-                for colidx, template_cell in enumerate(rows, start=1):
-                    current_cell = ws.cell(row = rowidx, column = colidx)
-                    copied_style = template_cell.style
-                    current_cell.value = template_cell.value
-                    current_cell.style = copied_style
-                    start += 25
-
-    start_row = 33
-    flightSegments = len(flightSegmentData)
-    while flightSegments > 1:
-        start_row += 25
-        zipForm(flightSegments)
-        flightSegments -= 1
-
-#    def lookUpFunction(matchobj):
-#        resp = data[str(matchobj.group(2)).lower()]
-#        if resp:
-#            return resp
-#        return 'key not found'
-#
-#    for row in ws.iter_rows():
-#        for cell in row:
-#            if cell.value is not None:
-#                cell.value = re.sub(pattern, lookUpFunction, cell.value.lower())
-#
     wb.save('/Users/kyleblazepetan/Desktop/mergeTesting.xlsx')
-    return True
--- segmentAdder.py
+++ segmentAdder.py
@@ -1,13 +1,11 @@
 from openpyxl import load_workbook
-from itertools import *
 import re
 
 wb = load_workbook('/Users/kyleblazepetan/Desktop/mailMergeTemplate.xlsx')
 ws = wb.active
-pattern = re.compile(r'FLIGHT SEGMENT ')
 
-def segmentAdder(flightSegments):
-
+def segmentAdder(flightSegments, ws):
+    pattern = re.compile(r'FLIGHT SEGMENT ')
     passedin = len(flightSegments)
     segments = 55 + ( 23 * passedin )
     for index in xrange(55, segments, 23):
@@ -21,10 +19,7 @@ def segmentAdder(flightSegments):
                 if cell.value is not None:
                     val = cell.value
                     if pattern.search(val) is not None:
-                        print('real_good')
                         bl_cell.value = "Flight Segment " +  str(crazy_inner_count)
-                        print crazy_inner_count
-                        print 'foo'
                     else:
                         bl_cell.value = cell.value
                 else:
